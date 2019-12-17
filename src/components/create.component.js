8a232d041d3b1548eb1834597e960ce6a1e9cfca
Alexander Tesliuk <teslo2004@mail.ru>
2019-12-17 17:56:56 +0300

    validators

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,7 @@
 import {Component} from '../core/component';
 import {Form} from '../core/form';
+import {Validators} from '../core/validators';
+
 
 export class CreateComponent extends Component{
     constructor(id){
@@ -12,17 +14,24 @@ export class CreateComponent extends Component{
         this.$el.addEventListener('submit', submitHandler.bind(this));
 
         this.form = new Form(this.$el, {
-            title: [],
-            fulltext: []
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
         });
     }
 }
 
 function submitHandler(event){
     event.preventDefault();
-    const formData = {
-        type: this.$el.type.value,
-        ...this.form.value()
+
+    if (this.form.isValid()){
+        const formData = {
+            type: this.$el.type.value,
+            ...this.form.value()
+        }
+        console.log('Submit', formData)
     }
-    console.log('Submit', formData)
+    else{
+        console.log('Warning!!!')
+    }
+    
 }
