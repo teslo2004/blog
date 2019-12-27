278014dcbf2b58f596238afd335ab360b3e30c60
teslo2004 <teslo2004@gmail.com>
2019-12-27 23:34:30 +0200

    add firebase

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,13 +1,12 @@
 import {Component} from '../core/component';
 import {Form} from '../core/form';
 import {Validators} from '../core/validators';
+import { apiService } from '../services/api.services';
 
 
 export class CreateComponent extends Component{
     constructor(id){
         super(id)
-
-        //this.form = null
     }
 
     init(){
@@ -20,18 +19,19 @@ export class CreateComponent extends Component{
     }
 }
 
-function submitHandler(event){
+async function submitHandler(event){
     event.preventDefault();
 
     if (this.form.isValid()){
         const formData = {
             type: this.$el.type.value,
+            date: new Date().toLocaleDateString(),
             ...this.form.value()
         }
 
-        this.form.clear()
+        await apiService.createPost(formData)
 
-        console.log('Submit', formData)
+        this.form.clear()
     }
     
     
