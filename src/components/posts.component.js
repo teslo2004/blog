4b738fffb5b06a9eade13a8a82b1379fbe6431c6
teslo2004 <teslo2004@gmail.com>
2020-01-09 13:52:26 +0200

    add loader

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -3,12 +3,15 @@ import {apiService} from '../services/api.services';
 import { TransformService } from '../services/transform.service'
 
 export class PostsComponent extends Component{
-    constructor(id){
+    constructor(id, {loader}){
         super(id)
+        this.loader = loader
     }
 async onShow(){
+    this.loader.show();
     const fbData = await apiService.fetchPosts();
     const posts = TransformService.fbObjectToArray(fbData);
+    this.loader.hide();
     const html = posts.map(post => renderPost(post));
 
     this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
